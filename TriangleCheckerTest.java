ae4f205b16dd1a07bd800df3136db498d07225ef
repl.it user <replituser@example.com>
2020-11-11 18:12:35 +0000

    Assignment4tdd2

--- TriangleCheckerTest.java
+++ TriangleCheckerTest.java
@@ -18,5 +18,49 @@ public class TriangleCheckerTest {
         assertEquals(TriangleChecker.TriangleType.NORMAL, type);
 
     }
+    @Test
+    public void testEquilateralTriangle() {
 
+      // Arrange
+        float a = 3;
+        float b = 3;
+        float c = 3;
+
+      // Act
+      TriangleChecker.TriangleType type = TriangleChecker.checkTriangle(a, b, c);
+
+      // Assert
+      assertEquals(TriangleChecker.TriangleType.EQUILATERAL, type);
+    }
+
+    @Test
+    public void testNoneTriangle() {
+
+      // Arrange
+        float a = 10;
+        float b = 4;
+        float c = 5;
+
+      // Act
+      TriangleChecker.TriangleType type = TriangleChecker.checkTriangle(a, b, c);
+
+      // Assert
+      assertEquals(TriangleChecker.TriangleType.NONE, type);
+    }
+
+    @Test
+    public void testIsoscelesTriangle() {
+
+      // Arrange
+        float a = 6;
+        float b = 6;
+        float c = 5;
+
+      // Act
+      TriangleChecker.TriangleType type = TriangleChecker.checkTriangle(a, b, c);
+
+      // Assert
+      assertEquals(TriangleChecker.TriangleType.ISOSCELES, type);
+    }
+    
 }
